83a75822162ac814fea24bfc4bad6b5622b1513f
kkulykk <kkulykk@gmail.com>
2022-02-14 23:45:18 +0200

    Update script

--- scripts/measure_methods.py
+++ scripts/measure_methods.py
@@ -11,17 +11,17 @@ def main():
     input_file = sys.argv[2]
     output_file = sys.argv[3]
 
-    methods = 3
+    methods = 5
     eps = 1 * 10 ** (-8)
 
-    times = [0 for _ in range(methods)]
+    result = {"stringstream": 0, "stod": 0, "sscanf": 0, "boost": 0, "Qt": 0}
 
     for m in range(methods):
         prev_val, cur_val = (0, 0), (0, 0)
-        small_time = float('inf')
+        smallest_time = float('inf')
 
         for i in range(repetitions):
-            proc = Popen(["./converter", str(m + 1),
+            proc = Popen(["./bin/converter", str(m + 1),
                          input_file, output_file], stdout=PIPE, stderr=PIPE)
 
             (time, error) = proc.communicate()
@@ -34,8 +34,8 @@ def main():
             error = str(error)
             time = int(time)
 
-            if small_time > time:
-                small_time = time
+            if smallest_time > time:
+                smallest_time = time
 
             prev_val = cur_val
             with open(output_file, "r") as f:
@@ -45,16 +45,15 @@ def main():
             if (prev_val[0] - cur_val[0] > eps) or (prev_val[1] - cur_val[1] > eps):
                 print(f"Different results for method {m + 1}")
 
-        times[m] = small_time
-    print_res(times)
+        result[list(result.keys())[m]] = smallest_time
+    print_res(result)
 
 
-def print_res(times):
-    print(f"String to double convertion, us:\n"
-          f"Using stringstream: {times[0]}\n"
-          f"Using stod: {times[1]}\n"
-          f"Using sscanf: {times[2]}\n\n"
-          f"All results are the same.\n")
+def print_res(res):
+    print("\nString to double convertion, us:\n")
+    for i in sorted(res.items(), key=lambda x: x[1]):
+        print(f"Using {i[0]}: {i[1]}")
+    print("\nAll results are the same.\n")
 
 
 main()
